823166a78a84a3650c9dc6b026a3a0da36d33212
jeremyyang824 <jeremyyang824@aliyun.com>
2016-06-23 17:31:53 +0800

    add repository

---
+++ IRepository.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Emico.Data.Repository
+{
+    public interface IRepository
+    {
+    }
+
+    public interface IRepository<TEntity> : IRepository
+        where TEntity : class
+    {
+        TEntity Add(TEntity entity);
+        TEntity Remove(TEntity entity);
+    }
+}
---
+++ IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Emico.Data.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int Commit();
+        T GetRepository<T>() where T : IRepository;
+    }
+}
